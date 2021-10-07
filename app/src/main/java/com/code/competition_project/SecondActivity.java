5813e319beb3a605a0a8cc68568d8bde5f9d6fe9
DongMin-0604 <dongmin3147@gmail.com>
2021-10-07 18:18:32 +0900

    매니페스트 수정

--- app/src/main/java/com/code/competition_project/SecondActivity.java
+++ app/src/main/java/com/code/competition_project/SecondActivity.java
@@ -1,14 +1,32 @@
 package com.code.competition_project;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
+    ImageButton QR_code_BT;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        QR_code_BT = findViewById(R.id.QR_code_bt);
+
+        QR_code_BT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"버튼 작동",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
