224c1a1c7ba9393872ffe1af83d77fc222f7447a
DongMin-0604 <dongmin3147@gmail.com>
2021-10-07 18:08:54 +0900

    메인화면 xml 절반 완료,spinner 리스트 기능 추가,타이틀바 삭제

--- app/src/main/java/com/code/competition_project/MainActivity.java
+++ app/src/main/java/com/code/competition_project/MainActivity.java
@@ -3,12 +3,57 @@ package com.code.competition_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner SP_grade,SP_class,SP_number;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SP_grade = findViewById(R.id.SP_grade);
+        SP_class = findViewById(R.id.SP_class);
+        SP_number = findViewById(R.id.SP_number);
+
+        //array에 있던 학년 목록 값들을 List로 만듦
+        List<String> array_grade = Arrays.asList(getResources().getStringArray(R.array.array_grade));
+        List<String> array_class = Arrays.asList(getResources().getStringArray(R.array.array_class));
+        List<String> array_number = Arrays.asList(getResources().getStringArray(R.array.array_number));
+
+        //ArrayAdapter를 만들어서 학년 값을 XML에 스피너에 연결
+        ArrayAdapter<String> adapter_grade = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,array_grade);
+        adapter_grade.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        SP_grade.setAdapter(adapter_grade);
+
+        ArrayAdapter<String> adapter_class = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,array_class);
+        adapter_class .setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        SP_class.setAdapter(adapter_class);
+
+        ArrayAdapter<String> adapter_number = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,array_number);
+        adapter_number.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        SP_number.setAdapter(adapter_number);
+
+        //spinner 선택 시 이벤트
+        SP_grade.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
