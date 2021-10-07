756c01444172c1746b0b18a6523abe334a0e4a8e
DongMin-0604 <dongmin3147@gmail.com>
2021-10-07 16:43:15 +0900

    엑티비티 추가

---
+++ app/src/main/java/com/code/competition_project/ScanActivity.java
@@ -0,0 +1,14 @@
+package com.code.competition_project;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScanActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+}
