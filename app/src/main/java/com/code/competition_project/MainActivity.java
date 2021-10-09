44649b3a183796ffdc8a123d2f06d6aa8f7002f7
DongMin-0604 <dongmin3147@gmail.com>
2021-10-09 15:25:31 +0900

    중간커밋,메인 화면구성 변경(로고,정보 입력창),다음으로 버튼 클릭시 SecondActivity로 이동 구현,예외처리 완료

--- app/src/main/java/com/code/competition_project/MainActivity.java
+++ app/src/main/java/com/code/competition_project/MainActivity.java
@@ -2,11 +2,16 @@ package com.code.competition_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +19,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     Spinner SP_grade,SP_class,SP_number;
+    Button bt_next;
+    EditText et_name;
+    CheckBox cb_agree;
 
 
     @Override
@@ -24,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         SP_grade = findViewById(R.id.SP_grade);
         SP_class = findViewById(R.id.SP_class);
         SP_number = findViewById(R.id.SP_number);
+
+        bt_next = findViewById(R.id.BT_next);
+        et_name = findViewById(R.id.ET_name);
+        cb_agree = findViewById(R.id.CB_agree);
 
         //array에 있던 학년 목록 값들을 List로 만듦
         List<String> array_grade = Arrays.asList(getResources().getStringArray(R.array.array_grade));
@@ -43,17 +55,22 @@ public class MainActivity extends AppCompatActivity {
         adapter_number.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         SP_number.setAdapter(adapter_number);
 
-        //spinner 선택 시 이벤트
-        SP_grade.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //'다음으로' 버튼 영역
+        bt_next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onClick(View v) {
+                if (et_name.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"잘못된 입력입니다.",Toast.LENGTH_SHORT).show();
+                }else if (!cb_agree.isChecked()){
+                    Toast.makeText(getApplicationContext(),"개인정보 수집에 동의해주세요.",Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent_view_Change = new Intent(getApplicationContext(),SecondActivity.class);
+                    startActivity(intent_view_Change);
+                }
 
             }
         });
+
+
     }
 }
